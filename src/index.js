45edb171ecc1bd2c80581c7c45246b40bd5becf6
unknown <abdelhedi.hlel@oyez.fr>
2019-01-02 17:47:41 +0100

    add posts Query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+const posts = [
+  { id: 'jsdfsdf4521sdf', title: 'Post number 1 title ', body: 'Post number 1 body', published: true },
+  { id: 'jsdfsdsdfsf4521sdf', title: 'Post number 2 title', body: 'Post number 2 body', published: false },
+  { id: 'jsdfsdfsdf4521sdf', title: 'Post number 3 title', body: 'Post number 3 body', published: true }
+]
 const typeDefs = `
 type Query {
-  hello: String!
-  name : String!
   me: User!
   Post: Post!
+  add(x:Int!,y:Int!): Float!
+  posts(query:String): [Post!]!
 }
 type User{
   id: ID!
@@ -22,13 +27,13 @@ type Post{
 `
 const resolvers = {
   Query: {
-    hello: () => 'Bismilleh alrahmen alrahim',
-    name: () => 'abdelhedi',
     me: () => ({ id: '54df5g4d5fg4d5f4gdfg', name: 'abdelhedi', email: 'sjkjdfnksdf', age: 15 }),
-    Post: () => ({ id: '1245dsd45dfgsdf', title: 'Article title', body: 'This is tje content of the Article' })
+    Post: () => ({ id: '1245dsd45dfgsdf', title: 'Article title', body: 'This is tje content of the Article' }),
+    add: (_, { x, y }) => (x + y) * 10,
+    posts: (_, { query }) => posts.filter(post => post.title.toLowerCase().includes(query) || post.body.includes(query))
   }
 }
 const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => {
-  console.log('Server stared ! ')
+server.start(({ port }) => {
+  console.log(`Server started at ${port}`)
 })
